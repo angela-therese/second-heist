5cce3804b764334466542d7b78361a52417ab44e
User-42 <a.t.iafrate@gmail.com>
2021-11-18 20:26:21 -0500

    phase 1 through phase 2 #3

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace heist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List <Member> TeamMembers = new List<Member>();
+           
+          
+          
+          
+            Console.WriteLine("Plan your heist!");
+
+            while(true)
+            {
+            Console.WriteLine("Enter your name.");
+            string memberName = Console.ReadLine();
+
+            if (memberName == "")
+            {
+                break;
+            }
+            Console.WriteLine("Enter your skill level (1-10)");
+            string skillLevel = Console.ReadLine();
+            int intSkillLevel = Convert.ToInt32(skillLevel);
+            Console.WriteLine("Enter your courage factor (0.0-2.0)");
+            string courageFactor = Console.ReadLine();
+            double decCourageFactor = Convert.ToDouble(courageFactor);
+
+
+            TeamMembers.Add(new Member{
+
+                Name = ($"{memberName}"),
+                SkillLevel = intSkillLevel,
+                Courage = decCourageFactor
+
+            });
+           
+
+            // Console.WriteLine("Player Information");
+            // Console.WriteLine("--------------");
+            // Console.WriteLine(memberName);
+            // Console.WriteLine(intSkillLevel);
+            // Console.WriteLine(decCourageFactor);
+
+        } }
+    }
+}
