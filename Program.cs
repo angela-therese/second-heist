22693b286204911deb14d5a33715b55f322ff313
User-42 <a.t.iafrate@gmail.com>
2021-11-21 21:24:10 -0500

    completed Phase 3

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace heist
            
           
           
-          
+            int teamSkill = 0;
             Console.WriteLine("Plan your heist!");
 
             while(true)
@@ -23,12 +23,14 @@ namespace heist
             {
                 break;
             }
-            Console.WriteLine("Enter your skill level (1-10)");
+            Console.WriteLine("Enter your skill level (1-50)");
             string skillLevel = Console.ReadLine();
             int intSkillLevel = Convert.ToInt32(skillLevel);
+            teamSkill = (teamSkill + intSkillLevel);
             Console.WriteLine("Enter your courage factor (0.0-2.0)");
             string courageFactor = Console.ReadLine();
             double decCourageFactor = Convert.ToDouble(courageFactor);
+
 
 
             TeamMembers.Add(new Member{
@@ -38,6 +40,35 @@ namespace heist
                 Courage = decCourageFactor
 
             });
+          
+            }
+
+            int TeamSize = TeamMembers.Count;
+            int BankLevel = 100;
+
+            Console.WriteLine($"{TeamSize} team members");
+            Console.WriteLine($"Total Team Skill Score: {teamSkill}");
+            
+            if(BankLevel < teamSkill){
+                Console.WriteLine("Success! Your skill level exceed the bank's difficulty.");
+            }
+            else{
+                Console.WriteLine("Uh-oh! Bank difficulty is greater than your skill level. You're in trouble!");
+            }
+
+
+
+
+
+            //DISPLAY MEMBERS' INFORMATION
+            // Console.WriteLine("************");
+            // foreach(Member t in TeamMembers){
+            //     Console.WriteLine(t.Name);
+            //     Console.WriteLine($"Skill Level: {t.SkillLevel}");
+            //     Console.WriteLine($"Courage: {t.Courage}");
+            //     Console.WriteLine("--------------------");
+
+            // }
            
 
             // Console.WriteLine("Player Information");
@@ -46,6 +77,6 @@ namespace heist
             // Console.WriteLine(intSkillLevel);
             // Console.WriteLine(decCourageFactor);
 
-        } }
+         }
     }
 }
