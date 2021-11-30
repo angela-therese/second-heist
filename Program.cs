3ab635e516eb72d64451d53ed38d25a6c76478c6
User-42 <a.t.iafrate@gmail.com>
2021-11-29 21:46:13 -0500

    completed heist part 1

--- Program.cs
+++ Program.cs
@@ -43,39 +43,49 @@ namespace heist
           
             }
 
-            int TeamSize = TeamMembers.Count;
-            int BankLevel = 100;
+           
+            Console.WriteLine("How many times do you want to try the heist?");
+            string trialRunsString = Console.ReadLine();
+            int trialRuns = Convert.ToInt32(trialRunsString);
+            int trials = 0;
+            int heistSuccess = 0;
+            int heistFail = 0;
 
-            Console.WriteLine($"{TeamSize} team members");
-            Console.WriteLine($"Total Team Skill Score: {teamSkill}");
-            
-            if(BankLevel < teamSkill){
-                Console.WriteLine("Success! Your skill level exceed the bank's difficulty.");
-            }
-            else{
-                Console.WriteLine("Uh-oh! Bank difficulty is greater than your skill level. You're in trouble!");
-            }
+            while (trials < trialRuns){
 
-
-
-
-
-            //DISPLAY MEMBERS' INFORMATION
-            // Console.WriteLine("************");
-            // foreach(Member t in TeamMembers){
-            //     Console.WriteLine(t.Name);
-            //     Console.WriteLine($"Skill Level: {t.SkillLevel}");
-            //     Console.WriteLine($"Courage: {t.Courage}");
-            //     Console.WriteLine("--------------------");
-
-            // }
            
 
-            // Console.WriteLine("Player Information");
-            // Console.WriteLine("--------------");
-            // Console.WriteLine(memberName);
-            // Console.WriteLine(intSkillLevel);
-            // Console.WriteLine(decCourageFactor);
+            int TeamSize = TeamMembers.Count;
+            Console.WriteLine("What is the bank's difficulty level from 1-100?");
+            string BankLevelString = Console.ReadLine();
+            int BankLevel = Convert.ToInt32(BankLevelString);
+            int LuckValue = new Random().Next(-10, 11);
+            int NewBankLevel = (BankLevel + LuckValue);
+            Console.WriteLine($"Factoring in luck, your bank's difficulty level is now {NewBankLevel}");
+            Console.WriteLine($"{TeamSize} team members");
+            Console.WriteLine($"Total Team Skill Score: {teamSkill}");
+            Console.WriteLine($"Bank difficulty level: {NewBankLevel}");
+            
+            
+            if(BankLevel < teamSkill){
+                Console.WriteLine("Success! You win!");
+                heistSuccess += 1;
+            }
+            else{
+                Console.WriteLine("Uh-oh! You lose!");
+                heistFail += 1; 
+            }
+
+            trials += 1;
+
+            }
+
+            Console.WriteLine($"You won the heist {heistSuccess} times.");
+            Console.WriteLine($"You lost the heist {heistFail} times.");
+           
+
+            
+
 
          }
     }
